4b721a5d7daf9e543ae581b2c6ee6ace5553aa22
kitakun <kitakunx@gmail.com>
2020-02-15 14:33:37 +0700

    * Added overlay on creating new record  * Added metatags for social networks

--- Kitakun.TagDiary.Web/Startup.cs
+++ Kitakun.TagDiary.Web/Startup.cs
@@ -81,7 +81,7 @@
                 // The default HSTS value is 30 days.
                 // You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-                //app.UseHttpsRedirection();
+                app.UseHttpsRedirection();
             }
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
